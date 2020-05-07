8517b9fe91a9f3f4debc92eb25cb3a49cef7ed3f
Gang Yuan <gyuan@xilinx.com>
2020-05-08 02:47:16 +0800

    update

--- login.sh
+++ login.sh
@@ -2,12 +2,12 @@
 USER=nimbix
 echo "Running as $USER"
 echo "export PATH=/opt/vitis_ai/conda/bin:$PATH" >> /home/$USER/.bashrc
-echo "export VERSION=$VERSION" >> /home/$USER/.bashrc
+echo "export VERSION=1.1" >> /home/$USER/.bashrc
 echo ". /opt/vitis_ai/conda/etc/profile.d/conda.sh" >> /home/$USER/.bashrc
-echo "export DATE=\"$DATE\"" >> /home/$USER/.bashrc
+echo "export DATE=2020_05_08" >> /home/$USER/.bashrc
 echo "export VAI_ROOT=/opt/vitis_ai" >> /home/$USER/.bashrc
 echo "export PYTHONPATH=/opt/vitis_ai/compiler" >> /home/$USER/.bashrc
-echo "export LD_LIBRARY_PATH=/opt/xilinx/xrt/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:/opt/vitis_ai/conda/envs/vitis-ai-tensorflow/lib/" >> /home/$USER/.bashrc
+echo "export LD_LIBRARY_PATH=/opt/xilinx/xrt/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:/opt/vitis_ai/conda/envs/vitis-ai-tensorflow/lib" >> /home/$USER/.bashrc
 
 echo "/etc/banner.sh" >> /home/$USER/.bashrc
 cp -p /etc/README.txt /home/$USER/
